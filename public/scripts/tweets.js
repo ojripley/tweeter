d43a0e5083761e52c1ced7d18525196ebe5bbc3e
Owen Ripley <ojripley@gmail.com>
2019-10-08 10:24:44 -0400

    added icons to tweets

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-  $('.tweet').on('mouseover', function() {
+  // $('.tweet').on('mouseover', function() {
+  //   $(this).children('header').children('.username').css({ display: 'inline' });
+  //   $(this).css({ 'box-shadow': '7px 7px #4056a1' });
+  // });
+  // $('.tweet').on('mouseleave', function() {
+  //   $(this).children('header').children('.username').css({ display: 'none' });
+  //   $(this).css({ 'box-shadow': 'none' });
+  // });
+
+  
+  // hover accomplishes the same thing as the above, but takes two event handler functions to make it happen on mouseover AND mouseleave
+  $('.tweet').hover(function() {
     $(this).children('header').children('.username').css({ display: 'inline' });
     $(this).css({ 'box-shadow': '7px 7px #4056a1' });
-  });
-  $('.tweet').on('mouseleave', function() {
+  }, function() {
     $(this).children('header').children('.username').css({ display: 'none' });
     $(this).css({ 'box-shadow': 'none' });
   });
