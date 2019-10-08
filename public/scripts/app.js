0579560786d9db0da301e9be9947a2609f9c8b0b
Owen Ripley <ojripley@gmail.com>
2019-10-08 17:49:06 -0400

    added sliding error messages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const displayScrollToTop = function() {
+  
+};
+
 const submitNewTweet = function() {
   event.preventDefault();
 
+  $('#error-message').slideUp();
+
   if ($(event.target).children('textarea').val().length === 0) {
-    alert('Tweet submissons cannot be empty!');
+    $('#error-message').text('Tweet submissons cannot be empty!');
+    $('#error-message').slideDown();
   } else if ($(event.target).children('textarea').val().length > 140) {
-    alert('Tweet submissons must be 140 characters or shorter!');
+    $('#error-message').text('Tweet submissons must be 140 characters or less!');
+    $('#error-message').slideDown();
   } else {
     const serializedForm = $(event.target).serialize();
 
@@ -22,27 +30,37 @@ const submitNewTweet = function() {
     
 };
 
-const dayDifference = function(dateInMilliseconds) {
-  // returns number of days passed since date
-  return Math.floor(((Date.now() - dateInMilliseconds) / 86400000));
+const ageString = function(dateInMilliseconds) {
+  
+  const difference = (Date.now() - dateInMilliseconds);
+
+  if (difference >= 86400000) {
+    return `${Math.floor(difference / 86400000)} days ago`;
+  } else if (difference >= 3600000) {
+    return `${Math.floor(difference / 3600000)} hours ago`;
+  } else if (difference >= 60000) {
+    return `${Math.floor(difference / 60000)} minutes ago`;
+  } else {
+    return `${Math.floor(difference / 1000)} seconds ago`;
+  }
 };
 
 const createTweetElement = function(tweetObject) {
   // use jQuery to create new article element
   const $tweet = $('<article>').addClass('tweet');
 
-  // determine age of tweet in days
-  const tweetAge = dayDifference(tweetObject.created_at);
+  // determine age of tweet
+  const tweetAge = ageString(tweetObject.created_at);
 
   // append elements as children to article
   $tweet.append(`<header>
       <span class="user-icon"><i class="fa fa-user-circle-o" aria-hidden="true"></i></span>
-      <span class="tweetedBy">${tweetObject.user.name}</span>
-      <span class="username">${tweetObject.user.handle}</span>
+      <span class="tweetedBy">${practiceSafeText(tweetObject.user.name)}</span>
+      <span class="username">${practiceSafeText(tweetObject.user.handle)}</span>
       </header>
-      <div class="content">${tweetObject.content.text}</div>
+      <div class="content">${practiceSafeText(tweetObject.content.text)}</div>
       <footer>
-      <span class="date">${tweetAge} days ago</span>
+      <span class="date">${tweetAge}</span>
       <a class="flag-button" href="flag"><i class="fa fa-flag" aria-hidden="true"></i></a>
       <a class="like-button" href="like"><i class="fa fa-heart" aria-hidden="true"></i></a>
       <a class="retweet-button" href="http://www.twitter.com"><i class="fa fa-retweet" aria-hidden="true"></i></a>
@@ -52,10 +70,18 @@ const createTweetElement = function(tweetObject) {
   return $tweet;
 };
 
+const practiceSafeText = function(string) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(string));
+  return div.innerHTML;
+};
+
+
 const renderTweets = function(tweetArray) {
   $('#tweets').empty();
   // append each tweet to tweets container
   for (let i = tweetArray.length - 1; i >= 0; i--) {
+    
     $('#tweets').append(createTweetElement(tweetArray[i]));
   }
 };
@@ -63,18 +89,35 @@ const renderTweets = function(tweetArray) {
 const loadTweets = function() {
   $.ajax('/tweets', {method: 'GET'})
     .then((tweets) => {
-      // const arrayOfTweets = JSON.parse(tweets);
-      // console.log(arrayOfTweets);
       renderTweets(tweets);
     });
 };
 
 $(document).ready(() => {
   
-  
+  loadTweets();
+
   $('.new-tweet-form').on('submit', (event) => {
     submitNewTweet(event);
   });
 
-  loadTweets();
+  $('.new-tweet-button').on('click', () => {
+    console.log('clicked');
+    $('.new-tweet').slideToggle();
+  });
+
+  // $(window).resize(function(e) {
+  //   console.log(e);
+  // });
+
+  $(window).scroll(function(event) {
+    const scrollPosition = $(window).scrollTop();
+    if (scrollPosition > 400) {
+      displayScrollToTop();
+    } else if (scrollPosition < 400) {
+      hideScrollToTop();
+    }
+  });
+
+  
 });
--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  // $('.tweet').on('mouseover', function() {
-  //   $(this).children('header').children('.username').css({ display: 'inline' });
-  //   $(this).css({ 'box-shadow': '7px 7px #4056a1' });
-  // });
-  // $('.tweet').on('mouseleave', function() {
-  //   $(this).children('header').children('.username').css({ display: 'none' });
-  //   $(this).css({ 'box-shadow': 'none' });
-  // });
-
-  
-  // hover accomplishes the same thing as the above, but takes two event handler functions to make it happen on mouseover AND mouseleave
-  $('.tweet').hover(function() {
+  $('#tweets').on('mouseover', '.tweet', function() {
     $(this).children('header').children('.username').css({ display: 'inline' });
     $(this).css({ 'box-shadow': '7px 7px #4056a1' });
-  }, function() {
+  });
+  $('#tweets').on('mouseleave', '.tweet', function() {
     $(this).children('header').children('.username').css({ display: 'none' });
     $(this).css({ 'box-shadow': 'none' });
   });
+
+  
+  // hover accomplishes the same thing as the above, but takes two event handler functions to make it happen on mouseover AND mouseleave
+  // $('#tweets .tweet').hover(function() {
+  //   $(this).children('header').children('.username').css({ display: 'inline' });
+  //   $(this).css({ 'box-shadow': '7px 7px #4056a1' });
+  // }, function() {
+  //   $(this).children('header').children('.username').css({ display: 'none' });
+  //   $(this).css({ 'box-shadow': 'none' });
+  // });
 });
