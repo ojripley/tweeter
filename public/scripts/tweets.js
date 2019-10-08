0a2902d59b3ccafac622df59f189ae65b87769e5
Owen Ripley <ojripley@gmail.com>
2019-10-08 09:38:46 -0400

    properly formatted tweet contents. added box-shadow on mouseover

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('.tweet').on('mouseover', function() {
-    $('.username').css({ display: 'inline' });
+    $(this).children('header').children('.username').css({ display: 'inline' });
+    $(this).css({ 'box-shadow': '7px 7px #4056a1' });
   });
   $('.tweet').on('mouseleave', function() {
-    $('.username').css({ display: 'none' });
+    $(this).children('header').children('.username').css({ display: 'none' });
+    $(this).css({ 'box-shadow': 'none' });
   });
 });
