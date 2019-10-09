5b228c958adddc3c9dc44ef9c574912300ae2855
Owen Ripley <ojripley@gmail.com>
2019-10-09 18:35:31 -0400

    changed .catch statements to .fail for ajax requests, as .catch is deprecated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -59,7 +59,7 @@ const submitNewTweet = function() {
       .then(() => {
         loadTweets();
       })
-      .catch(() => {
+      .fail(() => {
         console.log(`Error loading tweets`);
       });
   }
@@ -156,7 +156,7 @@ const loadTweets = function() {
     .then((tweets) => {
       renderTweets(tweets);
     })
-    .catch(() => {
+    .fail(() => {
       console.log(`Error loading tweets`);
     });
 };
